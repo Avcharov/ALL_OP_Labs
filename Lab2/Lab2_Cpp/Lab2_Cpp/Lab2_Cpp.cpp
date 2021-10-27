e23b55bedf3009f6a098734d338bbe182ffda6a6
Avcharov <72924156+Avcharov@users.noreply.github.com>
2021-10-27 14:36:47 +0300

    Add files via upload

---
+++ Lab2/Lab2_Cpp/Lab2_Cpp/Lab2_Cpp.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+    int x, y;
+    cout << "Input x, y";
+    cin >> x >> y;
+    if ((y >= x - 1 && x <= 0 && y >= 1) || (y >= x - 1 && x >= 1 && y <= 0))
+        cout << "Yes"; 
+    else
+        cout << "No";
+
+    return 0;
+}
---
+++ Lab2/Lab2_Python/Lab2_Python/Lab2_Python.py
@@ -0,0 +1,6 @@
+import math
+x=int(input("Введіть х"))
+y=int(input("Введіть у"))
+if (y>=x-1 and x<=0 and y>=1) or (y>=x-1 and x>=1 and y<=0):
+    print("Yes") 
+else: print("No")
