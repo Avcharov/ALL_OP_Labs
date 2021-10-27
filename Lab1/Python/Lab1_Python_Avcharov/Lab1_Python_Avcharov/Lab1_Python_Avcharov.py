8f9aaa9fe24a6085c8bf7c0dcec34bcb8733fe36
Avcharov <72924156+Avcharov@users.noreply.github.com>
2021-10-27 14:29:46 +0300

    Delete Lab1/Python directory

--- Lab1/Python/Lab1_Python_Avcharov/Lab1_Python_Avcharov/Lab1_Python_Avcharov.py
+++
@@ -1,8 +0,0 @@
-import math
-x=int(input("Введіть х"))
-y=int(input("Введіть у"))
-if (y>=x-1 and x<=0 and y>=1):
-    print("Yes") 
-elif (y>=x-1 and x>=1 and y<=0): 
-    print("Yes")
-else: print("No")
