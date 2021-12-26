07ff429d7043e680c7c3af9cf001fc360f165b98
Avcharov <72924156+Avcharov@users.noreply.github.com>
2021-12-26 16:50:48 +0200

    Add files via upload

---
+++ Lab9/Lab9_Cpp/Lab9_Cpp/Lab9_Cpp.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <string>
+#include <sstream> 
+using namespace std;
+
+int main()
+{
+    int count = 0;
+    string s, word;
+    int n;cout << "String:\n"; 
+    getline(cin, s);
+    cout << "Input N - ";
+    cin >> n;
+    
+   
+    stringstream words(s);
+    while (words >> word)
+    {
+        if (word.size() < n) { cout << word << "\n"; count++; }
+    }
+
+    system("pause");
+    return 0;
+}
